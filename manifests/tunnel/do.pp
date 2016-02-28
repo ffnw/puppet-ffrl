16476dc2412a8e3c05a6aa5333c561c957ceb18f
Simon Kurka <dev@simon.kurka.cc>
2016-02-28 16:56:25 +0100

    some syntax

--- manifests/tunnel/do.pp
+++ manifests/tunnel/do.pp
@@ -10,17 +10,16 @@ define ffrl::tunnel::do (
     validate_ip_address($remote_public_ip)
     validate_ip_address($nat_ip)
 
+    $_transfer_net = ip_network($transfer_net)
+    $_nat_ip = ip_address($nat_ip)
+
     $pre_up    = [ '/sbin/ip rule add pref 31000 iif $IFACE table 42',
                    '/sbin/ip rule add pref 31001 iif $IFACE unreachable',
-                   '/sbin/iptables -t nat -A POSTROUTING'
-                   + ' ! -s ' + ip_network($transfer_net) + ' -o $IFACE'
-                   + ' -j SNAT --to-source ' + ip_address($nat_ip) ]
+                   '/sbin/iptables -t nat -A POSTROUTING ! -s ${_transfer_net} -o $IFACE -j SNAT --to-source ${_nat_ip}' ]
     $post_up   = [ '/sbin/sysctl -w net.ipv4.conf.$IFACE.rp_filter=0' ]
     $post_down = [ '/sbin/ip rule del pref 31000 iif $IFACE table 42',
                    '/sbin/ip rule del pref 31001 iif $IFACE unreachable',
-                   '/sbin/iptables -t nat -D POSTROUTING'
-                   + ' ! -s ' + ip_network($transfer_net) + ' -o $IFACE'
-                   + ' -j SNAT --to-source ' + ip_address($nat_ip) ]
+                   '/sbin/iptables -t nat -D POSTROUTING ! -s ${_transfer_net} -o $IFACE -j SNAT --to-source ${_nat_ip}' ]
 
     network::inet::tunnel { "gre-ffrl-${title}":
       address   => ip_network($transfer_net, 1),
