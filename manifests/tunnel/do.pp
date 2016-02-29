9423947a4c48721a427977185e93060840c4edff
Simon Kurka <dev@simon.kurka.cc>
2016-02-29 21:07:50 +0100

    renamed some vars in order to be replaced by their values

--- manifests/tunnel/do.pp
+++ manifests/tunnel/do.pp
@@ -14,8 +14,8 @@ define ffrl::tunnel::do (
     validate_ip_address($remote_public_ip)
     validate_ip_address($nat_ip)
 
-    $_transfer_net = ip_network($transfer_net)
-    $_nat_ip = ip_address($nat_ip)
+    $f_transfer_net = ip_network($transfer_net)
+    $f_nat_ip = ip_address($nat_ip)
 
     if ($kernel_table) {
       $ip4_rule_up   = [ "/bin/ip -4 rule add pref 31000 iif \$IFACE table ${kernel_table}",
@@ -33,9 +33,9 @@ define ffrl::tunnel::do (
       $ip6_rule_down = []
     }
 
-    $pre_up    = [ '/sbin/iptables -t nat -A POSTROUTING ! -s ${_transfer_net} -o $IFACE -j SNAT --to-source ${_nat_ip}' ] + $ip4_rule_up
+    $pre_up    = [ '/sbin/iptables -t nat -A POSTROUTING ! -s ${f_transfer_net} -o $IFACE -j SNAT --to-source ${f_nat_ip}' ] + $ip4_rule_up
     $post_up   = [ '/sbin/sysctl -w net.ipv4.conf.$IFACE.rp_filter=0' ]
-    $post_down = [ '/sbin/iptables -t nat -D POSTROUTING ! -s ${_transfer_net} -o $IFACE -j SNAT --to-source ${_nat_ip}' ] + $ip4_rule_down
+    $post_down = [ '/sbin/iptables -t nat -D POSTROUTING ! -s ${f_transfer_net} -o $IFACE -j SNAT --to-source ${f_nat_ip}' ] + $ip4_rule_down
 
     network::inet::tunnel { "gre-ffrl-${title}":
       address   => ip_network($transfer_net, 1),
