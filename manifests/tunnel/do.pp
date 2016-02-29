2e40c8d13fdd82a9cd8ee74cd97303c9f4f7706f
Simon Kurka <dev@simon.kurka.cc>
2016-02-29 21:39:08 +0100

    some net to ip and clamp pmtu foo

--- manifests/tunnel/do.pp
+++ manifests/tunnel/do.pp
@@ -32,15 +32,17 @@ define ffrl::tunnel::do (
       $ip6_rule_down = []
     }
 
-    $pre_up    = [ "/sbin/iptables -t nat -A POSTROUTING ! -s ${_transfer_net} -o \$IFACE -j SNAT --to-source ${_nat_ip}" ] + $ip4_rule_up
+    $pre_up    = [ "/sbin/iptables -t nat -A POSTROUTING ! -s ${_transfer_net} -o \$IFACE -j SNAT --to-source ${_nat_ip}",
+                   "/sbin/iptables -A FORWARD -o \$IFACE -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu", ] + $ip4_rule_up
     $post_up   = [ "/sbin/sysctl -w net.ipv4.conf.\$IFACE.rp_filter=0" ]
-    $post_down = [ "/sbin/iptables -t nat -D POSTROUTING ! -s ${_transfer_net} -o \$IFACE -j SNAT --to-source ${_nat_ip}" ] + $ip4_rule_down
+    $post_down = [ "/sbin/iptables -t nat -D POSTROUTING ! -s ${_transfer_net} -o \$IFACE -j SNAT --to-source ${_nat_ip}",
+                   "/sbin/iptables -D FORWARD -o \$IFACE -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu", ] + $ip4_rule_down
 
     network::inet::tunnel { "gre-ffrl-${title}":
-      address   => ip_network($transfer_net, 1),
+      address   => ip_address(ip_network($transfer_net, 1)),
       mode      => 'gre',
       endpoint  => ip_address($remote_public_ip),
-      dstaddr   => ip_network($transfer_net, 0),
+      dstaddr   => ip_address(ip_network($transfer_net, 0)),
       ttl       => 255,
       mtu       => 1476,
       pre_up    => $pre_up,
